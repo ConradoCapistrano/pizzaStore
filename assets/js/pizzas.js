8412641153ed088cbc89f61966a3fa3cdaefe4d0
Conrado Capistrano <capistrano.conrado@gmail.com>
2023-02-08 17:46:06 -0300

    first commit

---
+++ assets/js/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'./assets/images/pizza.png', price:63.90, sizes:['100g', '530g', '860g'], description:'Queijo mussarela, molho de tomate, oregano e azeite de oliva'},
+    {id:2, name:'Calabresa', img:'./assets/images/pizza2.png', price:70.90, sizes:['320g', '530g', '860g'], description:'Calabresa defumada, queijo mussarela, azeitonas e oregano'},
+    {id:3, name:'Quatro Queijos', img:'./assets/images/pizza3.png', price:79.90, sizes:['320g', '530g', '860g'], description:'Queijo parmesão, mussarela, ricota e oregano'},
+    {id:4, name:'Portuguesa', img:'./assets/images/pizza4.png', price:81.90, sizes:['320g', '530g', '860g'], description:'Presunto, ovos cozidos, milho, ervilha, mussarela e oregano'},
+    {id:5, name:'Atum', img:'./assets/images/pizza5.png', price:90.90, sizes:['320g', '530g', '860g'], description:'Atum, queijo mussarela, aneis de cebola e oregano'},
+    {id:6, name:'Moda da Casa', img:'./assets/images/pizza6.png', price:84.90, sizes:['320g', '530g', '860g'], description:'Charque desfiado, aneis de cebola, queijo mussarela e oregano'},
+    {id:7, name:'Chocolate', img:'./assets/images/pizza7.png', price:90.90, sizes:['320g', '530g', '860g'], description:'Base de mussarela com brigadeiro cremoso e granulado'}
+];
